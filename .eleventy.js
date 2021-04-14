0fe1bd5fdbdbb4de3c4b0a2406c54c2735d4e7d4
Jewell Scott <jewelljrscott@gmail.com>
2021-04-13 22:33:21 -0400

    add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
+
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
@@ -6,9 +9,13 @@ const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 
 module.exports = config => {
 
-  // Add filters
+// Add filters
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('w3DateFilter', w3DateFilter);
+
+  // Plugins
+config.addPlugin(rssPlugin);
+
 
   config.addFilter("randomItem", (arr) => {
     arr.sort(() => {
