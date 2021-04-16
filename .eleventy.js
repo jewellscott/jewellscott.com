62a2b7168caa7a3a9c6314b14f8cd8fe32e17864
Jewell Scott <jewelljrscott@gmail.com>
2021-04-15 22:34:36 -0400

    add --serve/browsersync 404 routing

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const fs = require("fs");
 
 
 // Filters
@@ -15,6 +16,24 @@ module.exports = config => {
 
   // Plugins
 config.addPlugin(rssPlugin);
+
+  // add Browsersync 404 routing
+
+  config.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
 
   config.addFilter("randomItem", (arr) => {
