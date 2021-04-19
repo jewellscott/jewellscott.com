10092359fade08319d5aa7f4aa230982ea7ee839
Jewell Scott <jewelljrscott@gmail.com>
2021-04-19 16:41:34 -0400

    WIP remove all traces of project stuff

--- .eleventy.js
+++ .eleventy.js
@@ -51,18 +51,6 @@ config.addPlugin(rssPlugin);
   return [...collection.getFilteredByGlob('./src/blog/*.md')].reverse();
   });
 
-  // Returns project items, sorted by display order
-  config.addCollection('projects', collection => {
-  return sortByDisplayOrder(collection.getFilteredByGlob('./src/projects/*.md'));
-  });
-
-  // Returns work items, sorted by display order then filtered by featured
-  config.addCollection('featuredProjects', collection => {
-  return sortByDisplayOrder(collection.getFilteredByGlob('./src/projects/*.md')).filter(
-    x => x.data.featured
-  );
-});
-
 // Set directories to pass through to the dist folder
 config.addPassthroughCopy('./src/images/');
   
--- src/pages/projects.md
+++
@@ -1,6 +0,0 @@
----
-title: 'Projects'
-layout: 'layouts/projects.html'
----
-
-This is an even more lovely short blurb on the subject, which is going to be longer than the title.
